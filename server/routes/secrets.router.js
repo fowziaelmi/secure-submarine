77669b097b166b5cd77b6d0610c307573b22f83f
Faduma Elmi <fowziiae@gmail.com>
2021-03-10 15:47:42 -0600

    fixed secret route so that only people with clearance can see

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
+  if (req.user.clearance_level !== '10') {
+    res.sendStatus(403);
+    return;
+  }
   pool
     .query(`SELECT * FROM "secret";`)
     .then((results) => res.send(results.rows))
